a081ed3a28431648973375126601f2adb7ad378e
chic-caller-7520 <arbrown110@gmail.com>
2020-04-17 05:19:37 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,33 +1,34 @@
-class Artist 
-  attr_accessor :name 
-  
-  @@all= []
-  
+class Artist
+  attr_accessor :name
+
+  @@all = []
+
   def initialize(name)
     @name = name
     @@all << self
   end
+
   def self.all
     @@all
   end
-  
+
   def add_song(song)
-    song.artist = self 
-  end
-  
-  def song
-    Song.all.select{|song| song.artist == self}
-  end
-  
-  def song_by_name (name)
-    song= Song.new(name)
     song.artist = self
   end
-  
-  def song_count
-    Song.all.count 
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
   end
-end  
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def self.song_count
+    Song.all.count
+  end
+end 
   
     
     
