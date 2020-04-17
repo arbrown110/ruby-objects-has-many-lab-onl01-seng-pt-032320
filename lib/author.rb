3a32ef30b0b17ce0efb6e60ae927e2d84b9e724e
chic-caller-7520 <arbrown110@gmail.com>
2020-04-17 05:25:07 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -11,21 +11,21 @@ class Author
     @@all
   end
   
-  def add_post(post)
-    post.author = self 
-  end
-  
-  def posts
-    Post.all.select{|post| post.author == self}
-  end
-  
-  def add_post_by_title (title)
-    post= Post.new(title)
+    def add_post(post)
     post.author = self
   end
-  
-  def post_count
-    Post.all.count 
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
   end
-end  
+
+  def posts
+    Post.all.select {|post| post.author == self}
+  end
+
+  def self.post_count
+    Post.all.count
+  end
+end
   
