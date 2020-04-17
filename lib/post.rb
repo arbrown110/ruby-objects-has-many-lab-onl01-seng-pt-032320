70d5ee2a8ed3bdb3a79e7bafc9af829b9cfb009b
chic-caller-7520 <arbrown110@gmail.com>
2020-04-17 05:13:39 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,14 +1,16 @@
 class Post
-  attr_accessor :title,:name 
+  attr_accessor :title,:author 
   
   @@all= []
   
-  def initialize(name)
-    @name = name
+  def initialize(title)
+    @title= title
     @@all << self
   end
   def self.all
     @@all
   end
+  
+  
   
  
